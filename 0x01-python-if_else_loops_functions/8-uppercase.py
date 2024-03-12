9bc45eea656bd411836a9fe18d5bd4143f6645d5
Abdullah ubuntu <abdullah@ubuntu.com>
2024-03-12 22:08:55 +0200

    updated 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str: str):
     newone = ''
     for letter in str:
         newone += chr(ord(letter)-32) if 97 <= ord(letter) <= 122 else letter
-    return newone
+    print("{}".format(newone))
