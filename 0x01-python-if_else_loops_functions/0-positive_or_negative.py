68d57cd95b70df5986e7e3223b437413f80ca368
Abdullah ubuntu <abdullah@ubuntu.com>
2024-03-12 21:21:30 +0200

    update indentation 0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10, 10)
 x = ''
 if number > 0:
-	x = "is positive"
+    x = "is positive"
 elif number == 0:
-	x = "is zero"
+    x = "is zero"
 else:
-	x = "is negative"
+    x = "is negative"
 print(f"{number:d} {x}")
